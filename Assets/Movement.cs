d25908de7e83c853c474b3b02b470635579d3ee9
LbsCristian <cristian.ingvarsson@elev.ga.lbs.se>
2024-01-09 14:53:40 +0100

    Added player movement and jumping

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb;
+    public int MovementSpeed = 5;
+    public int JumpForce = 10;
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*MovementSpeed, rb.velocity.y); 
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
