9168fc698a14baeae103ce96f15b5aca2752e262
LbsCristian <cristian.ingvarsson@elev.ga.lbs.se>
2024-01-10 16:22:21 +0100

    idk

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,28 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private const double V = -8.5;
+
     // Start is called before the first frame update
     Rigidbody2D rb;
     public int MovementSpeed = 5;
-    int JumpForce;
-    public int DeafultJumpForce = 15;
-
-    BoxCollider2D bc;
-
-    [SerializeField]
-    BoxCollider2D WaterTrigger;
-
+    public int JumpForce = 10;
+    public GameObject Enemy;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        bc = gameObject.GetComponent<BoxCollider2D>();
-        bc.size = new Vector2(1, 1);
-        bc.offset = new Vector2(0, 0f);
-        JumpForce = DeafultJumpForce;
     }
 
     // Update is called once per frame
@@ -32,33 +23,17 @@ public class Movement : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z)&& rb.velocity.y==0)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
-        if (Input.GetKeyUp(KeyCode.Z)&&rb.velocity.y>0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y/2);
-        }
-
-        if (WaterTrigger.enabled == false)
-        {
-            JumpForce = DeafultJumpForce / 2;
-            bc.size = new Vector2(1, 2);
-            bc.offset = new Vector2(0, 0.5f);
-        }
-        else
-        {
-            JumpForce = DeafultJumpForce;
-            bc.size = new Vector2(1, 1);
-            bc.offset = new Vector2(0, 0);
-        }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D otherObj)
     {
-        if (collision.name == "Water")
+        if (otherObj.gameObject.tag == "Enemy")
         {
-            
+            Debug.Log("Collided and died");
+            transform.position = new Vector2(-8, 1);
         }
     }
 }
